21a44a05430bdfcaa6df8b738fe721dcffb36298
Almaz <q19.almaz@gmail.com>
2025-04-21 12:57:57 +0500

    new migration

--- core/settings.py
+++ core/settings.py
@@ -69,7 +69,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
