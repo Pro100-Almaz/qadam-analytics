b55a077ada191c752822a3bdc908a3ce134fc173
Argon0ut <nusupoversultan228@gmail.com>
2025-05-03 18:48:05 +0500

    Changed status coloring

--- apps/authentication/models.py
+++ apps/authentication/models.py
@@ -16,6 +16,7 @@ def user_avatar_upload_path(instance, filename):
     hex_digest = hash_object.hexdigest()
 
     filename = f"avatar_{hex_digest}.{ext}"
+    print("hello kitty")
     return os.path.join('user_avatars', str(instance.email), filename)
 
 class CustomUser(AbstractUser):
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -48,6 +48,7 @@ def register_user(request):
 
     if request.method == "POST":
         avatar_file = request.FILES.get('avatar')
+        print(avatar_file)
         form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
