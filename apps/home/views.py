3fb156117a70013bb7e0887e0437865764bc85ac
Almaz <q19.almaz@gmail.com>
2025-05-06 11:44:12 +0500

    added: lesson and subject details

--- apps/home/views.py
+++ apps/home/views.py
@@ -4,11 +4,11 @@ from django.contrib import messages
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse, reverse_lazy
+from django.shortcuts import render, redirect, get_object_or_404
 
-from .forms import LessonForm, LessonGroupForm
-from .models import Lesson, StudentGrade
-from ..authentication.models import CustomUser
-from django.shortcuts import render, redirect
+from apps.home.forms import LessonForm, LessonGroupForm
+from apps.home.models import Lesson, StudentGrade
+from apps.authentication.models import CustomUser
 
 
 @login_required(login_url="/login/")
@@ -59,7 +59,7 @@ def lessons_list(request):
     lessons = Lesson.objects.all()
     number_of_students = {}
     for lesson in lessons:
-        number_of_students[lesson] = lesson.students.count()
+        number_of_students[lesson] = CustomUser.objects.filter(classroom=lesson.subject.classroom).count()
     context = {'lessons': lessons, "number_of_students": number_of_students}
     html_template = loader.get_template('home/lessons.html')
 
@@ -117,3 +117,15 @@ def lesson_create(request):
         form = LessonForm()
 
     return render(request, "home/new_lesson.html", {"form": form})
+
+
+@login_required(login_url="/login/")
+def lesson_details(request, pk):
+    lesson = get_object_or_404(Lesson, pk=pk)
+    student_grades = StudentGrade.objects.filter(lesson=lesson)
+    
+    context = {
+        'lesson': lesson,
+        'student_grades': student_grades,
+    }
+    return render(request, 'home/lesson_details.html', context)
