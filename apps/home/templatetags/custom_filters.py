d6a23ef7195838c746d14d94faa2cb1dab0dc8b2
Almaz <q19.almaz@gmail.com>
2025-05-16 21:22:24 +0500

    updated project

--- apps/home/templatetags/custom_filters.py
+++ apps/home/templatetags/custom_filters.py
@@ -8,4 +8,9 @@ def get_item(dictionary, key):
     Template filter to get an item from a dictionary using a key
     Usage: {{ dictionary|get_item:key }}
     """
-    return dictionary.get(key, {}) 
+    if not dictionary:
+        return None
+    try:
+        return dictionary.get(key)
+    except (AttributeError, TypeError):
+        return None 
