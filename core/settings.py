0ee2d7e32b194070d58295aa77b945d73523e792
Almaz <q19.almaz@gmail.com>
2025-05-23 12:47:25 +0200

    working version

--- core/settings.py
+++ core/settings.py
@@ -81,6 +81,13 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': 'db.sqlite3',
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
