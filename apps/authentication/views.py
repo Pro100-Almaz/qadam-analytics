cae28bae553a2f49266cdc295f51c6d2ab0cf97e
Argon0ut <nusupoversultan228@gmail.com>
2025-05-30 22:28:16 +0500

    adding password changing feature

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render, redirect
+import random
+
+from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail, EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.core.signing import Signer
 
-from .forms import LoginForm, SignUpForm
+from core import settings
+from .forms import LoginForm, SignUpForm, ForgetPasswordForm, VerificationPasswordForm
 from .models import CustomUser
 from errors import find_error_by_key
+
 
 
 def login_view(request):
@@ -66,9 +75,86 @@ def register_user(request):
 
     return render(request, "accounts/register.html", context)
 
+def forget_password_view(request):
+    form = ForgetPasswordForm(request.POST or None)
+    context = {"form": form}
+
+    if request.method == "POST":
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            return  send_email_password_change(request, username)
+    return render(request, 'accounts/forget_password.html', context)
+
+
+def send_email_password_change(request, username):
+    user = CustomUser.objects.filter(username=username).first()
+    if not user:
+        return redirect('authentication:login')
+
+
+    verification_code = random.randint(100000, 999999)
+
+    subject = "Verification email"
+    message = render_to_string("email/verification_email.html", {"user": user, "verification_code": verification_code})
+    from_mail = settings.DEFAULT_FROM_EMAIL
+    to_mail = [user.email]
+
+    email = EmailMultiAlternatives(subject, "", from_mail, to_mail)
+    email.attach_alternative(message, "text/html")
+    email.send()
+
+    signer = Signer()
+    signed_code = signer.sign(str(verification_code))
+
+    return redirect("verification_code", username=user.username, signed_code=signed_code)
+
+#
+def verification_code_check(request, username, signed_code):
+    user = get_object_or_404(CustomUser, username=username)
+    form = VerificationPasswordForm(request.POST or None)
+    signer = Signer()
+
+    if request.method == "POST":
+
+        entered = request.POST.get("verification_code")
+        if not entered.isdigit():
+            form.add_error('verification_code', "Неверный код.")
+            return render(request, "accounts/verification_waitlist.html", {"form": form})
+
+        entered = int(entered)
+        actual = int(signer.unsign(signed_code))
+
+        if entered != actual:
+            form.add_error('verification_code', "Неверный код.")
+        else:
+            return redirect("password_change", username=user.username, signed_code=signed_code)
+
+    return render(request, "accounts/verification_waitlist.html", {"form": form})
+
+def password_change_final(request, username, signed_code):
+    user = get_object_or_404(CustomUser, username=username)
+    form = VerificationPasswordForm(request.POST or None)
+
+    if request.method == "POST":
+        pw1 = request.POST.get("password1")
+        pw2 = request.POST.get("password2")
+        print("NNNNNNNNNNNNNNNNNNNNNNNNNNNNN")
+
+        if pw1 != pw2:
+            messages.error(request, "Пароли не совпадают.")
+        else:
+            user.set_password(pw1)
+            user.save()
+            print("EEEEEEEEEEEEEEEEEEEEEEEEe")
+            return redirect("login")
+
+    return render(request, "accounts/password_change_final.html", {"form": form})
+
 
 @login_required
 def custom_logout_view(request):
+    form = ForgetPasswordForm()
     logout(request)
 
     return redirect('/login/')
+
