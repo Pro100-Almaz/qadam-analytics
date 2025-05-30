49a6b60ec6b5ff307651128b13fa5548ad6d0347
Almaz <q19.almaz@gmail.com>
2025-05-30 17:01:05 +0500

    added: new sidenav design and functionality

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, re_path
 from apps.home import views, subject
 
 urlpatterns = [
-    path('', views.welcome, name='welcome'),  # Add welcome page as default path
     path('profile/', views.profile, name='profile'),
     path('students', views.students_list, name='students'),
     path('students/<int:pk>', views.student_details, name='student_details'),
