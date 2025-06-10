cb0ddd7978cd1de74038d1223eb36645231ccccb
Almaz <q19.almaz@gmail.com>
2025-06-10 10:24:11 +0500

    added new models to admin page

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -4,6 +4,8 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 from django.contrib import admin
-from apps.authentication.models import CustomUser
+from apps.authentication.models import CustomUser, SchoolGroup, PsychologicalState
 
 admin.site.register(CustomUser)
+admin.site.register(SchoolGroup)
+admin.site.register(PsychologicalState)
