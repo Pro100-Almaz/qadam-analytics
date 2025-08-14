b1b788733f72fdbfcb0a58e103a77637d39ed55a
Argon0ut <nusupoversultan228@gmail.com>
2025-08-14 09:06:46 +0500

    fix_minor changes in subject details

--- apps/lesson/models.py
+++ apps/lesson/models.py
@@ -102,7 +102,7 @@ class Topic(models.Model):
         related_name='subtopics',
         on_delete=models.CASCADE
     )
-    title = models.CharField(max_length=255, unique=True)
+    title = models.CharField(max_length=255, unique=False)
     weight = models.FloatField(
         default=0.0,
         help_text="Percent weight in lesson (must sum to 100% for all siblings under same parent)"
