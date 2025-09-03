e9cbee6f08d3fb08be529b0126cd237242562dfc
Almaz <q19.almaz@gmail.com>
2025-09-03 16:00:45 +0500

    updated the local development logic

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,6 @@
 import os
+from http.cookiejar import debug
+
 from decouple import config
 from unipath import Path
 
@@ -102,16 +104,29 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432', cast=int),
+
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'qadam_dev',
+            'USER': 'yersultan',
+            'PASSWORD': '76429',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST', default='localhost'),
+            'PORT': config('DB_PORT', default='5432', cast=int),
+        }
+    }
 
 # DATABASES = {
 #     'default': {
