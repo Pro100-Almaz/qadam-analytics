ae349cd47efc499b3d70d88f813faba86f820a46
Argon0ut <nusupoversultan228@gmail.com>
2025-09-25 08:02:45 +0500

    adding UserAdmin management for users

--- apps/authentication/admin.py
+++ apps/authentication/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from apps.authentication.models import CustomUser, SchoolGroup, PsychologicalState, Supervisor, Teacher, Parent, Student
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("username", "email", "role", "is_staff")
+    search_fields = ("username", "email", "first_name", "last_name")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Personal info",
+         {"fields": ("first_name", "last_name", "email", "phone_number", "address", "school", "role")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("date_of_birth", "last_login")}),
+    )
 
 admin.site.register(Student)
 admin.site.register(Parent)
 admin.site.register(Teacher)
 admin.site.register(Supervisor)
 
-admin.site.register(CustomUser)
 admin.site.register(SchoolGroup)
 admin.site.register(PsychologicalState)
