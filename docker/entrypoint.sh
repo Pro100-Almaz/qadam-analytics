8b3dc2d2c006f9323b6bf2530c0fba468fb5ded0
Almaz <q19.almaz@gmail.com>
2025-10-18 17:48:39 +0500

    added new docker configuration for the run

---
+++ docker/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+# Wait for DB
+if [ -n "$DB_HOST" ]; then
+  echo "Waiting for Postgres at $DB_HOST:$DB_PORT..."
+  while ! nc -z "$DB_HOST" "$DB_PORT"; do sleep 1; done
+fi
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+# Start Gunicorn
+exec gunicorn core.wsgi:application \
+  --bind 0.0.0.0:8000 \
+  --workers ${GUNICORN_WORKERS:-3} \
+  --timeout ${GUNICORN_TIMEOUT:-30} \
+  --access-logfile '-' \
+  --error-logfile '-'
