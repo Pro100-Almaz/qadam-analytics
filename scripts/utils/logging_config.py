53a3afdf5f0636e38d3745004f7d9ee8609ef6db
Argon0ut <nusupoversultan228@gmail.com>
2025-11-08 23:18:59 +0500

    adding users according to the role

---
+++ scripts/utils/logging_config.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger('')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename="student_logging.log")
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
+
