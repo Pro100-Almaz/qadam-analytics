3e7a9ca7f888cd9b78dcd25440b7b48936dfa51c
Almaz <q19.almaz@gmail.com>
2026-01-25 09:14:42 +0500

    updated nav.py for layers sidenav

--- apps/home/templatetags/nav.py
+++ apps/home/templatetags/nav.py
@@ -2,19 +2,26 @@ from django import template
 
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
 def can_see_students(context):
     user = context["user"]
-    return user.is_authenticated and (user.is_manager or user.is_superuser or user.is_teacher)
+    if not user.is_authenticated:
+        return False
+    return user.is_manager() or user.is_superuser or user.is_teacher()
+
 
 @register.simple_tag(takes_context=True)
 def can_add_user(context):
     user = context["user"]
-    return user.is_authenticated and (user.is_manager or user.is_superuser)
+    if not user.is_authenticated:
+        return False
+    return user.is_manager() or user.is_superuser
+
 
 @register.simple_tag(takes_context=True)
 def parent_students(context):
     user = context["user"]
-    if user.is_authenticated and getattr(user, "is_parent", False):
-        return getattr(user, "get_students", lambda: [])()
+    if user.is_authenticated and user.is_parent():
+        return user.get_students() or []
     return []
