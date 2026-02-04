df4beac5c87d7d04266f118c91d7a64144813910
Argon0ut <nusupoversultan228@gmail.com>
2026-02-04 10:51:10 +0500

    fixing comment display logic

--- apps/lesson/views.py
+++ apps/lesson/views.py
@@ -457,7 +457,7 @@ def grading(request, pk):
         grades_map[(grade["topic_id"], grade["student_id"])] = grade["grade"]
 
 
-    # Comments merged/selecteed
+    # Comments merged/selected
     merged_comments = (
         MergedLessonComment.objects
         .filter(lesson=lesson, student__in=students)
@@ -475,7 +475,7 @@ def grading(request, pk):
         .values("student__user_id","topic__title", "comment").exclude(comment__isnull=True).exclude(comment="")
     )
     selected_comments_map = {
-        selected_comment["student__user_id"]: f"{selected_comment['topic__title']}: {selected_comment['comment']}" for selected_comment in selected_comments
+        selected_comment["student__user_id"]: selected_comment['comment'] for selected_comment in selected_comments
     }
 
     comment_modes = {}
@@ -548,7 +548,7 @@ def submit_all_topic_grades(request):
 
             top_comment = request.POST.get(f'topic_{topic.id}_comment', '').strip()
             if comment_mode == "merged":
-                toMerge += f"{topic.title}: {top_comment} \n"
+                toMerge += f"{top_comment} \n\n"
 
 
             for sub in subtopics:
@@ -560,7 +560,7 @@ def submit_all_topic_grades(request):
                             'comment_selected': bool(request.POST.get(f'subtopic_{sub.id}_comment_selected', ''))}
 
                 if comment_mode == "merged":
-                    toMerge += f"{sub.title}: " + sub_defaults['comment'] + '\n'
+                    toMerge += sub_defaults['comment'] + '\n\n'
 
                 TopicGrade.objects.update_or_create(
                     student=student,
