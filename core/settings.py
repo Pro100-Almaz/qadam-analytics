62184ed202f9a364f02c34283eee9960495cbd54
Bekzhan <bekzhan@MacBook-Pro-Bekzhan.local>
2026-09-11 08:33:13 +0500

    password reset & student list distinct

--- core/settings.py
+++ core/settings.py
@@ -244,8 +244,8 @@ STATICFILES_DIRS = [os.path.join(CORE_DIR, "apps/static")]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
